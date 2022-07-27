b81faebea5c7cdeeb355c8a507b7275e6c9d7528
eweb-ossy <ossy2000@gmail.com>
2022-07-27 09:26:55 +0900

    add: news page

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 [トップページ](https://eweb-ossy.github.io/ios_ebisu_dining/)  
 [店舗情報](https://eweb-ossy.github.io/ios_ebisu_dining/restaurants.html)  
 [会社情報](https://eweb-ossy.github.io/ios_ebisu_dining/corporate_profile.html)  
+[お知らせ一覧](https://eweb-ossy.github.io/ios_ebisu_dining/news.html)  
---
+++ dist/news.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <title>お知らせ｜ゑびすダイニング</title>
+    <link rel="stylesheet" href="css/news.css?<?= time() ?>">
+</head>
+<body>
+<?php $page="news"; require('include/header.php'); ?>
+    <div class="mv-sub">
+        <div class="inner">
+            <h2>お知らせ<span>News & Topics</span></h2>
+            <div class="image">
+                <picture>
+                    <source media="(min-width: 1440px)" srcset="images/news_mv_photo.jpg">
+                    <source media="(max-width: 1439px)" srcset="images/news_mv_photo_sp.jpg">
+                    <img src="images/news_mv_photo.jpg" alt="">
+                </picture>
+            </div>
+        </div>
+    </div>
+    <section>
+        <div class="inner">
+            <div class="content">
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+                <a href="" class="news-block">
+                    <div class="news-date">2022.00.00</div>
+                    <div class="news-body">お知らせ入りますタイトルが入りますお知らせ入りますタイトルが入ります</div>
+                </a>
+            </div>
+            <nav class="pagination">
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="" class="prev"></a></li>
+                        <li><a href="" class="page-numbers active">1</a></li>
+                        <li><a href="" class="page-numbers">2</a></li>
+                        <li><a href="" class="page-numbers">3</a></li>
+                        <li><a href="" class="page-numbers">4</a></li>
+                        <li><a href="" class="page-numbers">5</a></li>
+                        <li><a href="" class="next"></a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </section>
+    <?php require('include/contact_part.php'); ?>
+    <?php require('include/footer_nav.php'); ?>
+    <?php require('include/footer.php'); ?>
+    <script src="js/news.js?<?= time() ?>"></script>
+</body>
+</html>
